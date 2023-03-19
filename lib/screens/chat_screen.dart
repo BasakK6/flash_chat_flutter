a083abc827b2731d1c36df125e89161b2034616a
BasakK6 <katukbasak@gmail.com>
2023-03-19 23:21:02 +0300

    feature: completed the messaging functionality & layout with firestore db

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,7 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flash_chat_flutter/components/message_bubble.dart';
+import 'package:flash_chat_flutter/components/messages_stream.dart';
 import 'package:flash_chat_flutter/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat_flutter/constants.dart';
+
+final _firestore = FirebaseFirestore.instance;
+late User loggedInUser;
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -14,7 +20,8 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
-  late User loggedInUser;
+  late String messageText;
+  final messageTextController = TextEditingController();
 
   void getCurrentUser() {
     try {
@@ -26,6 +33,25 @@ class _ChatScreenState extends State<ChatScreen> {
       print(e);
     }
   }
+
+  /*
+  void getMessages() async {
+    //.get() returns a Future<QuerySnapshot<Map<String, dynamic>>>
+    final messages = await _firestore.collection("messages").get();
+    for (var message in messages.docs) {
+      print(message.data()); //map
+    }
+  }
+
+  void messagesStream() {
+    //.snapshots() returns a Stream<QuerySnapshot<Map<String, dynamic>>>
+    _firestore.collection("messages").snapshots().forEach((snapshot) {
+      for (var doc in snapshot.docs) {
+        print(doc.data());
+      }
+    });
+  }
+  */
 
   @override
   void initState() {
@@ -59,6 +85,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            const MessagesStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -66,15 +93,23 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   TextButton(
                     onPressed: () {
+                      messageTextController.clear();
                       //Implement send functionality.
+                      //we have access to messageText + loggedInUser.email
+                      _firestore.collection("messages").add({
+                        "sender": loggedInUser.email,
+                        "text": messageText,
+                      });
                     },
                     child: const Text(
                       'Send',
@@ -87,6 +122,51 @@ class _ChatScreenState extends State<ChatScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MessagesStream extends StatelessWidget {
+  const MessagesStream({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection("messages").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final messages = snapshot.data!.docs.reversed; //reversed it in order the show the latest messages
+          List<MessageBubble> messageBubbles = [];
+          for (var message in messages) {
+            final messageText = message["text"];
+            final messageSender = message["sender"];
+            final currentUser = loggedInUser.email;
+
+            final messageBubble = MessageBubble(
+              text: messageText,
+              sender: messageSender,
+              isMe: currentUser == messageSender,
+            );
+            messageBubbles.add(messageBubble);
+          }
+          return Expanded(
+            child: ListView(
+              reverse: true, //makes the listview start from the end (to show the latest messages)
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 20,
+              ),
+              children: messageBubbles,
+            ),
+          );
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(
+            backgroundColor: Colors.lightBlueAccent,
+          ),
+        );
+      },
     );
   }
 }
